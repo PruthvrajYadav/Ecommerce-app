d17540c87a3a7816a1e67f7fd7804b64818fbf1c
gitDemo <sy9535393@gmail.com>
2025-07-29 18:58:33 +0530

    create SEO react and add image

--- cilent/src/Pages/HomePage.js
+++ cilent/src/Pages/HomePage.js
@@ -3,7 +3,7 @@ import Layout from '../components/layout/Layout'
 
 const HomePage = () => {
     return (
-        <Layout>
+        <Layout title={"Home page -Ecommerce App"}>
             <h1>HomePage</h1>
         </Layout>
     )
--- cilent/src/components/layout/Layout.js
+++ cilent/src/components/layout/Layout.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
+import { Helmet } from "react-helmet";
 
-const Layout = ({ children }) => {
+const Layout = ({ children, description, keywords, author, title }) => {
     return (
         <div>
+            <Helmet>
+                <meta charSet="UTF-8" />
+                <meta name="description" content={description} />
+                <meta name="keywords" content={keywords} />
+                <meta name="author" content={author} />
+                <title>{title}</title>
+            </Helmet>
             <Header />
-            <main style={{ minHeight: '80vh' }}>
+            <main style={{ minHeight: '70vh' }}>
                 {children}
             </main>
             <Footer />
