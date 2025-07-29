136f3c784638dbf37b475fcb9495f376d43ca330
gitDemo <sy9535393@gmail.com>
2025-07-29 17:00:52 +0530

    creating navigation and footer

--- cilent/src/Pages/Pagenotfound.js
+++ cilent/src/Pages/Pagenotfound.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import Layout from '../components/layout/Layout'
+import { Link } from 'react-router-dom'
 
 const Pagenotfound = () => {
     return (
         <Layout>
-            <h1>Pagenotfound</h1>
-        </Layout>
+            <div className='pnf'>
+                <h1 className='pnf-title'>404</h1>
+                <h2 className='pnf-heading'>Oops! Page not Found</h2>
+                <Link to='/' className='pnf-btn'>Go Back</Link>
+            </div>
+        </Layout >
     )
 }
 
